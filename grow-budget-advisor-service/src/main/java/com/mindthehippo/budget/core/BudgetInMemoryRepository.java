61697354bded27211daf9a5ae6d9ce308c88b943
morgade <morgade@gmail.com>
2016-10-22 14:54:13 -0300

    Changig repository to map of budgets

--- grow-budget-advisor-service/src/main/java/com/mindthehippo/budget/core/BudgetInMemoryRepository.java
+++ grow-budget-advisor-service/src/main/java/com/mindthehippo/budget/core/BudgetInMemoryRepository.java
@@ -1,15 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mindthehippo.budget.core;
 
 import com.mindthehippo.budget.aggregate.budget.Budget;
 import com.mindthehippo.budget.aggregate.budget.BudgetRepository;
 import com.mindthehippo.budget.aggregate.budget.Item;
 import com.mindthehippo.budget.aggregate.goal.Goal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import org.springframework.stereotype.Component;
 
@@ -20,16 +17,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class BudgetInMemoryRepository implements BudgetRepository {
 
-    List<Budget> budgets;
+    Map<UUID, Budget> budgets = new HashMap<>();
 
     @Override
     public List<Item> getItens(UUID account) {
-        for (Budget budget : budgets) {
-            if (budget.getAccount().equals(account)) {
-                return budget.getItems();
-            }
-        }
-        return null;
+        return budgets.get(account).getItems();
     }
 
     @Override
