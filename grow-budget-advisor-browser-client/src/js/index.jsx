13eace52286d0dc225c8a98941b4e9f93b724114
morgade <morgade@gmail.com>
2016-10-21 22:55:06 -0300

    added base index.jsx

---
+++ grow-budget-advisor-browser-client/src/js/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Index from 'file?name=[name].[ext]!../index.html';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
+
+var Hello = React.createClass({
+    render: function () {
+        return (
+                <div></div>
+                );
+    }
+});
