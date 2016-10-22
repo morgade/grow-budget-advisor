d004706159b301377e5089e5e7eb106d6f0ec055
morgade <morgade@gmail.com>
2016-10-22 17:43:44 -0300

    added interface navigation

--- grow-budget-advisor-service/src/main/java/com/mindthehippo/budget/interfaces/BudgetController.java
+++ grow-budget-advisor-service/src/main/java/com/mindthehippo/budget/interfaces/BudgetController.java
@@ -2,23 +2,33 @@ package com.mindthehippo.budget.interfaces;
 
 import com.mindthehippo.budget.application.BudgetApplicationService;
 import com.mindthehippo.budget.application.dto.BudgetDTO;
+import com.mindthehippo.budget.application.dto.ItemDTO;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Marcelo
  */
-//@RestController
+@RestController
 public class BudgetController {
 
     @Autowired
     private BudgetApplicationService budgetAppService;
 
     @RequestMapping(path = "/budget/{accountId}")
-    public BudgetDTO getItens(@PathVariable("accountId") UUID accountId) {
+    public BudgetDTO getBudget(@PathVariable("accountId") UUID accountId) {
         return budgetAppService.get(accountId);
     }
+
+    @RequestMapping(path = "/budget/{accountId}/item", method = RequestMethod.POST)
+    public void addItem(@PathVariable("accountId") UUID accountId, @RequestBody ItemDTO item) {
+        budgetAppService.store(accountId, item);
+    }
+    
 }
