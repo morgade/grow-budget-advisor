72234c8670846c95e9440dcb0d74897d37f73cef
morgade <morgade@gmail.com>
2016-10-21 22:14:18 -0300

    adding project packages

---
+++ grow-budget-advisor-service/src/main/java/com/mindthehippo/budget/aggregate/goal/Category.java
@@ -0,0 +1,12 @@
+package com.mindthehippo.budget.aggregate.goal;
+
+import java.util.UUID;
+
+/**
+ * Goal category value object
+ */
+public class Category {
+    private UUID id;
+    private String text;
+    
+}
