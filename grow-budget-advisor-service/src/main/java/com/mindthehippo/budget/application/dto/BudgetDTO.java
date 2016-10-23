cf9ef626aa367c6cc2f87958778d7f4b15503f8f
morgade <morgade@gmail.com>
2016-10-23 11:46:45 -0300

    expected balance

--- grow-budget-advisor-service/src/main/java/com/mindthehippo/budget/application/dto/BudgetDTO.java
+++ grow-budget-advisor-service/src/main/java/com/mindthehippo/budget/application/dto/BudgetDTO.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class BudgetDTO {
 
     private String account;
+    private Float expectedBalance;
     private List<ItemDTO> items;
     private List<GoalDTO> goals;
 
@@ -77,6 +78,14 @@ public class BudgetDTO {
 
     public void setGoalProgress(Map<String, Map<Integer, Float>> goalProgress) {
         this.goalProgress = goalProgress;
+    }
+
+    public Float getExpectedBalance() {
+        return expectedBalance;
+    }
+
+    public void setExpectedBalance(Float expectedBalance) {
+        this.expectedBalance = expectedBalance;
     }
 
 
